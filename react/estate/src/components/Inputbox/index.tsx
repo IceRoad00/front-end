d26f9fae2bebbde495f2570bad3998ad48103a5a
IceyoungK <anxis001220@gmail.com>
2024-04-11 14:07:11 +0900

    2024-04-11

--- react/estate/src/components/Inputbox/index.tsx
+++ react/estate/src/components/Inputbox/index.tsx
@@ -1,22 +1,37 @@
-// idInputBox, PasswordInputBox 함수형 컴포넌트 생성
-// 각각의 컨포넌트는 모두 export 가능하도록 내보내기
-import React from 'react';
+import React, { ChangeEvent } from "react";
 import './style.css';
 
 export interface InputBoxProps {
     label: string;
     type: 'text' | 'password' | 'email' | 'number';
+    value: string;
     placeholder: string;
+    onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
     buttonTitle?: string;
+    ButtonStatus?: boolean;
+    onButtonClickHandler?: () => void;
 }
 
-export default function InputBox({label, type, placeholder, buttonTitle}: InputBoxProps) {
+export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle, ButtonStatus, onButtonClickHandler}: InputBoxProps) {
+
+    const buttonClass = ButtonStatus ? 'input-primary-button' : 'input-disable-button';
+
     return (
         <div className="input-box">
             <div className="input-label label">{label}</div>
             <div className="input-content-box">
-                <input className="input" type={type} placeholder={placeholder} />
-                {buttonTitle && <div className="input-disable-button">{buttonTitle}</div>}
+                <input
+                    className="input"
+                    type={type}
+                    value={value}
+                    placeholder={placeholder}
+                    onChange={onChangeHandler}
+                />
+                {buttonTitle && 
+                <div className={buttonClass} onClick={onButtonClickHandler}>
+                    {buttonTitle}
+                </div> 
+                }
             </div>
             <div className="input-message"></div>
         </div>
